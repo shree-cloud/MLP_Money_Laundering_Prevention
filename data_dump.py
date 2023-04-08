2a0245e518b2ace98dd9532d099daf5eecb99893
shree <shreyashmohod@gmail.com>
2023-04-08 17:39:22 +0530

    00

---
+++ data_dump.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import pymongo
+import json
+
+# Provide the mongodb localhost url to connect python to mongodb.
+client = pymongo.MongoClient("mongodb://localhost:27017/neurolabDB")
+
+DATA_FILE_PATH = "/config/workspace/MLP_training_dataset.csv"
+DATABASE_NAME = "mlp"
+COLLECTION_NAME = "elleptic"
+
+if __name__=="__main__":
+    df = pd.read_csv(DATA_FILE_PATH)
+    df.drop('Unnamed: 0', axis=1,inplace=True)
+    print(f"rows and columns: {df.shape}")
+
+    #convert dataframe to json to dump these records in mongodb
+    df.reset_index(drop=True,inplace=True)
+
+    json_record = list(json.loads(df.T.to_json()).values())
+    print(json_record[0])
+
+    client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
